0c3ecbe0b56779394bb45c51be7b34cf2094c0eb
Nourhan Elsherif <nourhan.elsherif20@gmail.com>
2023-07-07 14:07:24 +0300

    Updated projects' details in myWork.json file

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,31 +2,34 @@ import React, { useContext } from 'react';
 import data from '../myWork/myWork.json';
 import styles from './Card.module.scss'
 import { Link } from 'react-router-dom';
-import {ImArrowUpRight2} from 'react-icons/im'
+import { ImArrowUpRight2 } from 'react-icons/im'
 import { DarkModeContext } from '../../context/ThemeContext';
 const WorkCard = () => {
-  const {darkMode} = useContext(DarkModeContext)
+  const { darkMode } = useContext(DarkModeContext)
   return (
-    <div style={{display:"flex", flexDirection:"row"}}>
-    
-    {data.map((i)=>{
-       return  (
-        <Link style={{textDecoration:"none"}} to={`/project/${i.id}`}><div className={darkMode ? styles.cardDark : styles.cardLight}>
-           <img className={styles.image} src={i.img} />
-           <div className={styles.title}>{i.name}</div>
-           <div className={styles.desc}>{i.subTitle}</div>
-           <br />
-           <Link to={`/project/${i.id}`}><button className={styles.btn}>View details <ImArrowUpRight2 /></button></Link>
-       </div></Link>
-      
-   )
-    })
+    <div style={{ display: "flex", flexDirection: "row" }}>
 
-    }
-       
+      {data.map((i) => {
+        return (
+          <div className={darkMode ? styles.cardDark : styles.cardLight}>
+            <a href={i.img} target="_blank">
+              <img className={styles.image} src={i.img} alt="project mockup image" />
+            </a>
+            <div className={styles.title}>{i.name}</div>
+            <div className={styles.desc}>{i.subTitle}</div>
+            <br />
+            <Link to={`/project/${i.id}`}><button className={styles.btn}>View details <ImArrowUpRight2 /></button></Link>
+          </div>
 
-</div>
-)}
+        )
+      })
+
+      }
+
+
+    </div>
+  )
+}
 
 
 export default WorkCard;
