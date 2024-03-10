468283f13361ace3da8e1db49b01d0ba67cea7d4
Nourhan Elsherif <nourhan.elsherif20@gmail.com>
2024-03-11 00:19:11 +0200

    update my data

--- src/components/whatIDo/WhatIdo.js
+++ src/components/whatIDo/WhatIdo.js
@@ -3,7 +3,7 @@ import styles from './WhatIdo.module.scss'
 import { DarkModeContext } from "../../context/ThemeContext";
 
 const WhatIdo = ({ sectionsRef }) => {
-    const {darkMode} = useContext(DarkModeContext)
+    const { darkMode } = useContext(DarkModeContext)
     return (
         <div ref={(ref) => (sectionsRef.current['section2'] = ref)} className={darkMode ? styles.whatDark : styles.whatLight}>
             <div>
@@ -12,7 +12,7 @@ const WhatIdo = ({ sectionsRef }) => {
             <br />
             <p className={styles.description}>
                 I’m a fresh graduate Computer Engineer, I specialize in <strong>Front-end development</strong>,
-                specifically in creating web applications using <strong>React JS</strong>. Additionally, I have some knowledge in backend development and a strong background in UI design.
+                specifically in creating web applications using <strong>Angular TS</strong> and <strong>React JS</strong>. Additionally, I have some knowledge in backend development and a strong background in UI design.
                 I possess expertise in designing user interfaces through tools like <strong >Adobe XD</strong> and <strong >Figma</strong>.
                 Beyond my technical skills, I have a passion for various forms of art and I actively engage
                 in digital painting as well. I am highly motivated to expand my knowledge and thrive on embracing
