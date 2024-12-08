05fc964d49ee8bb7b64faf0d6a48163a4a7355a5
Nourhan Elsherif <nourhan.elsherif@estafsar.com>
2024-12-08 18:26:14 +0200

    fic project name

--- src/pages/Error/Error404.js
+++ src/pages/Error/Error404.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from './Error404.module.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Error404.module.scss';
 
 const Error = () => {
-    return (
-        <div className={styles.error}>
-            <Link to="/"><img alt="Error 404 picture" className={styles.image} src="/404.svg" /></Link>
-        </div>
-    )
-}
-export default Error
+  return (
+    <div className={styles.error}>
+      <Link to="/">
+        <img alt="Error 404 picture" className={styles.image} src="/404.svg" />
+      </Link>
+    </div>
+  );
+};
+export default Error;
